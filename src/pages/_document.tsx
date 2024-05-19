c551c3a58b2a0d409ea2203fe89fe651466cd082
pssilva <pss1suporte@gmail.com>
2024-05-19 18:29:32 -0300

    {{ID_JIRA}} - Fatiando em compoenentes estruturando o Index Template HTML: Nível 1 - div.wrapper

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className={`hold-transition skin-blue sidebar-mini`}>
         <Main />
         <NextScript />
       </body>
